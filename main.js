bd23872e2b7e5e14efd3b9c17b1fb7b83a9dc5c8
MITANSH12 <78300422+MITANSH12@users.noreply.github.com>
2023-01-24 15:31:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+function setup() {
+canvas = createCanvas(480 , 400);
+canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+}
+function draw() {
+image(video , 0 , 0 , 480 , 400);
+}
+function start() {
+
+
+}
